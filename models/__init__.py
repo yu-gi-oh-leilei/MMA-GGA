0b3b174dcf2afb184e51335fda1aca051ffafb4d
yu-gi-oh-leilei <106001396+yu-gi-oh-leilei@users.noreply.github.com>
2022-07-09 21:21:35 +0800

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+from .models import *
+from .MMAGGA import *
+
+__factory = {
+    'AGW_Plus_Baseline': AGW_Plus_Baseline,
+    'MMAGGA': MMAGGA,
+}
+
+def get_names():
+    return __factory.keys()
+
+def init_model(name, *args, **kwargs):
+    if name not in __factory.keys():
+        raise KeyError("Unknown model: {}".format(name))
+    return __factory[name](*args, **kwargs)
